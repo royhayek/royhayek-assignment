0f20fa66fb26864407b0a391c288f59999943bcf
Roy <royhayek27@gmail.com>
2024-08-01 18:38:44 +0300

    Removed unecessary base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/royhayek-assignment/",
   define: {
     "process.env": process.env,
   },
